c61f07d8b47c8de50501954de69e85e09be185b9
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-10-19 14:20:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -56,7 +56,7 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'google.login'
     login_manager.init_app(app)
     
     
