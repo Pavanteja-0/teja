9eaaa79d347f6fb546a41632f40c7f09e72afff0
Pavan <52877475+Pavanteja-0@users.noreply.github.com>
2021-10-19 15:43:18 +0530

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ flow = Flow.from_client_secrets_file(
 )
 
     
-class User(db, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(256), unique=True)
     name = db.Column(db.String(256))
